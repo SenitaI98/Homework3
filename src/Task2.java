4a08be4c52d9ffc8039759081aa29ce425126b93
SenitaI98 <senita.isovic@live.com>
2022-03-02 00:00:07 +0100

    Update Task2

--- src/Task2.java
+++ src/Task2.java
@@ -6,16 +6,19 @@ public class Task2 {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         List<Integer> listOfNumbers = new ArrayList<>();
-        addNumbers((ArrayList<Integer>) listOfNumbers);
-        System.out.println("Do you want to add more numbers: y/n");
-        char yORn = reader.next().charAt(0);
-        if (yORn == 'y' || yORn == 'Y') {
-            addNumbers((ArrayList<Integer>) listOfNumbers);
+        detectNumber((ArrayList<Integer>) listOfNumbers);
+        char yORn = 'y';
+        while (yORn == 'y') {
+            System.out.println("Do you want to add more numbers: y/n");
+            yORn = reader.next().charAt(0);
+            if (yORn == 'n') {
+                break;
+            }
+            detectNumber((ArrayList<Integer>) listOfNumbers);
         }
-
     }
 
-    public static void addNumbers(ArrayList<Integer> listOfNumbers) {
+    public static void detectNumber(ArrayList<Integer> listOfNumbers) {
         Scanner reader = new Scanner(System.in);
         int number = 0;
         while (number != -10) {
