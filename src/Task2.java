43a8661b68ee7eb6fa33fe44a239e4dd65f081bb
SenitaI98 <senita.isovic@live.com>
2022-03-01 23:32:37 +0100

    Second way

--- src/Task2.java
+++ src/Task2.java
@@ -4,29 +4,27 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
-        int number = 0;
-        List<Integer> list = new ArrayList<Integer>();
-        while (number != -10){
-            Scanner reader = new Scanner(System.in);
-            System.out.println("Please enter any integer");
-            number = reader.nextInt();
-            list.add(number);
-            if (number == -10){
-                list.remove(list.size()-1);
-                for (int i = 0; i<list.size(); i++ ){
-                    System.out.print(list.get(i) + " ");
-                }
-                System.out.println("\nDo you want to add more numbers: y/n");
-                char yORn = reader.next().charAt(0);
-                if (yORn == 'y'){
-                    System.out.println("Please enter any integer");
-                    number = reader.nextInt();
-                    list.add(number);
-                } else if(yORn == 'n'){
-                    break;
-                }
-            }
-
+        Scanner reader = new Scanner(System.in);
+        List<Integer> listOfNumbers = new ArrayList<>();
+        addNumbers((ArrayList<Integer>) listOfNumbers);
+        System.out.println("Do you want to add more numbers: y/n");
+        char yORn = reader.next().charAt(0);
+        if (yORn == 'y' || yORn == 'Y') {
+            addNumbers((ArrayList<Integer>) listOfNumbers);
         }
+
+    }
+
+    public static void addNumbers(ArrayList<Integer> listOfNumbers) {
+        Scanner reader = new Scanner(System.in);
+        int number = 0;
+        while (number != -10) {
+            System.out.println("Please enter any integer different than -10");
+            number = reader.nextInt();
+            listOfNumbers.add(number);
+        }
+        listOfNumbers.remove(listOfNumbers.size() - 1);
+        System.out.println("You entered -10, your list look like this --> " + listOfNumbers);
     }
 }
+
